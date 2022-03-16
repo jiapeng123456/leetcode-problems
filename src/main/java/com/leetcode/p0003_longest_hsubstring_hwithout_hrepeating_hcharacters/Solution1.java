ab3a8a093d2370903cd7637f8cbf585ffcc0350f
hotdust <hotdust@163.com>
2022-03-16 09:58:19 +0800

    No.3:resolved

---
+++ src/main/java/com/leetcode/p0003_longest_hsubstring_hwithout_hrepeating_hcharacters/Solution1.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - p0003_longest_hsubstring_hwithout_hrepeating_hcharacters
+ */
+package com.leetcode.p0003_longest_hsubstring_hwithout_hrepeating_hcharacters;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+
+}
---
+++ src/main/java/com/leetcode/p0003_longest_hsubstring_hwithout_hrepeating_hcharacters/Solution2.java
@@ -0,0 +1,50 @@
+/**
+ * Leetcode - p0003_longest_hsubstring_hwithout_hrepeating_hcharacters
+ */
+package com.leetcode.p0003_longest_hsubstring_hwithout_hrepeating_hcharacters;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * 自己实现
+ *
+ *
+ */
+class Solution2 implements Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+
+        if (s.length() == 1) {
+            return 1;
+        }
+
+        Map<Character, Integer> window = new HashMap<>();
+        Map<Character, Integer> need = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+            window.put(c, 0);
+        }
+
+        int left = 0, right = 0;
+        int maxLen = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+
+            window.put(c, window.get(c) + 1);
+            while (window.get(c) > 1) {
+                char d = s.charAt(left);
+                left++;
+                window.put(d, window.get(d) - 1);
+            }
+
+            // 左边窗口收缩时，代表字符串长度要减少重复字符。
+            // 所以在减少后，再总记录长度。
+            maxLen = Integer.max(right - left, maxLen);
+        }
+
+        return maxLen;
+    }
+
+}
